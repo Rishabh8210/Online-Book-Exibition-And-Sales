c9dbab77eb2df609ef5c6d371ca94dd185398dad
Rishabh Pandey <rishabhpandey8092@gmail.com>
2024-08-16 15:50:35 +0530

    Resolving auto merged issue

--- Frontend/src/components/Subscription.js
+++ Frontend/src/components/Subscription.js
@@ -6,7 +6,7 @@ const Subscription = () => {
 
 
   return (
-    <div className='flex flex-col gap-10 md:flex-row justify-around p-3'>
+    <div className='flex w-full flex-col gap-10 md:flex-row md:justify-around justify-center items-center p-3 py-10'>
         {
             subscription && subscription.map((data, ind) => {
                 return <SubscriptionCard key={ind} subscription={data} index = {ind}/>
